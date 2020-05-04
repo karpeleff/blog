c3ea60e3e2bc60ee7e9beb7cdb11748fb6cb2f32
User <alekseykarpelev@gmail.com>
2020-05-04 18:28:12 +1000

    add !-- LikeBtn.com BEGIN

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -18,6 +18,14 @@
                         </div>
                     </div>
 
+                    <!-- LikeBtn.com BEGIN -->
+                    <span class="likebtn-wrapper" data-theme="review" data-lang="ru" data-ef_voting="grow" data-white_label="true" data-identifier="item_1" data-share_enabled="false" data-i18n_dislike="Не нравится"></span>
+                    <script>(function(d,e,s){if(d.getElementById("likebtn_wjs"))return;a=d.createElement(e);m=d.getElementsByTagName(e)[0];a.async=1;a.id="likebtn_wjs";a.src=s;m.parentNode.insertBefore(a, m)})(document,"script","//w.likebtn.com/js/w/widget.js");</script>
+                    <!-- LikeBtn.com END -->
+
+
+
+
                 </div>
             </div>
         </div>
